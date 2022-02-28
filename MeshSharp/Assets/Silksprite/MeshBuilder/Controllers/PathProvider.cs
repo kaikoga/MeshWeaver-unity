ce25a736fe9936a8469c220618c5174a15d35801
kaikoga <info@mail.kaikoga.net>
2022-02-28 20:05:56 +0900

    draw selection

--- MeshSharp/Assets/Silksprite/MeshBuilder/Controllers/PathProvider.cs
+++ MeshSharp/Assets/Silksprite/MeshBuilder/Controllers/PathProvider.cs
@@ -1,10 +1,16 @@
 using Silksprite.MeshBuilder.Controllers.Base;
 using Silksprite.MeshBuilder.Models;
+using UnityEngine;
 
 namespace Silksprite.MeshBuilder.Controllers
 {
     public abstract class PathProvider : GeometryProvider
     {
         public abstract Pathie ToPathie();
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawIcon(transform.position, "curvekeyframeselected", false); // it works!
+        }
     }
 }
