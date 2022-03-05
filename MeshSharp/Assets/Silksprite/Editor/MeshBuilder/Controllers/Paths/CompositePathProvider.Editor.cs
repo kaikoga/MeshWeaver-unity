7a4783adad548292734f4a0ea089292133d23aaa
kaikoga <info@mail.kaikoga.net>
2022-03-06 03:32:05 +0900

    remove unused code

--- MeshSharp/Assets/Silksprite/Editor/MeshBuilder/Controllers/Paths/CompositePathProvider.Editor.cs
+++ MeshSharp/Assets/Silksprite/Editor/MeshBuilder/Controllers/Paths/CompositePathProvider.Editor.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using Silksprite.MeshBuilder.Controllers.Base;
 using Silksprite.MeshBuilder.Controllers.Base.Modifiers;
 using Silksprite.MeshBuilder.Controllers.Paths.Modifiers;
@@ -22,33 +20,8 @@ namespace Silksprite.MeshBuilder.Controllers.Paths
             typeof(UvRemapperProvider)
         );
 
-        IEnumerable<VertexProvider> ActiveVertices()
-        {
-            return ((CompositePathProvider)target).GetComponentsInChildren<VertexProvider>().Where(c => c != null).ToArray();
-        }
-
         public override void OnInspectorGUI()
         {
-            void DistributeX(float min, float max)
-            {
-                var vertexProviders = ActiveVertices().ToList();
-                var iMax = vertexProviders.Count - 1;
-                foreach (var (v, i) in vertexProviders.Select((v, i) => (v, i)))
-                {
-                    v.uv.x = min + (max - min) * i / iMax;
-                }
-            }
-
-            void DistributeY(float min, float max)
-            {
-                var vertexProviders = ActiveVertices().ToList();
-                var iMax = vertexProviders.Count - 1;
-                foreach (var (v, i) in vertexProviders.Select((v, i) => (v, i)))
-                {
-                    v.uv.y = min + (max - min) * i / iMax;
-                }
-            }
-
             base.OnInspectorGUI();
             var compositePathProvider = (CompositePathProvider)target;
             PathProviderMenu.PropertyField(compositePathProvider, ref compositePathProvider.pathProviders);
