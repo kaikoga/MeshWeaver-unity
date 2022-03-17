085913013ebb992f3e7797eef3a95f1cb042cb89
kaikoga <info@mail.kaikoga.net>
2022-03-18 01:59:44 +0900

    better PropertyDrawer?

---
+++ MeshSharp/Assets/Silksprite/Editor/MeshBuilder/Models/DataObjects/Vector2ChannelData.Drawer.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Silksprite.MeshBuilder.Models.DataObjects
+{
+    [CustomPropertyDrawer(typeof(Vector2ChannelData))]
+    public class Vector2ChannelDataDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var oldLabelWidth = EditorGUIUtility.labelWidth;
+            GUI.Label(new Rect(position.xMin, position.yMin, oldLabelWidth, position.height), property.displayName);
+            position.xMin += oldLabelWidth;
+
+            var min = position.min;
+            var size = position.size;
+            var r1 = new Rect(min, size * new Vector2(0.2f, 1f));
+            var r2 = new Rect(min + size * new Vector2(0.2f, 0f), size * new Vector2(0.8f, 1f));
+
+            using (new EditorGUI.IndentLevelScope(1 - EditorGUI.indentLevel))
+            {
+                EditorGUI.PropertyField(r1, property.FindPropertyRelative("minIndex"), new GUIContent(""));
+                EditorGUI.PropertyField(r2, property.FindPropertyRelative("value"), new GUIContent(""));
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
