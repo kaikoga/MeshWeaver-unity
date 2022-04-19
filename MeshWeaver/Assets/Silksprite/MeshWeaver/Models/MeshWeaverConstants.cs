f95829a7c1f63c9886aba6aa55eeef9da65ead59
kaikoga <info@mail.kaikoga.net>
2022-04-20 05:23:22 +0900

    bump version

--- MeshWeaver/Assets/Silksprite/MeshWeaver/Models/MeshWeaverConstants.cs
+++ MeshWeaver/Assets/Silksprite/MeshWeaver/Models/MeshWeaverConstants.cs
@@ -2,6 +2,6 @@ namespace Silksprite.MeshWeaver.Models
 {
     public static class MeshWeaverConstants
     {
-        public const string Version = "0.2.0";
+        public const string Version = "0.2.1";
     }
 }
