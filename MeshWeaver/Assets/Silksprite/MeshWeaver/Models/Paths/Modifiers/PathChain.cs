9810e7fea041f24795d017874c7cac728dbb8353
kaikoga <info@mail.kaikoga.net>
2022-09-06 21:51:17 +0900

    remove dummy end from PathChain

--- MeshWeaver/Assets/Silksprite/MeshWeaver/Models/Paths/Modifiers/PathChain.cs
+++ MeshWeaver/Assets/Silksprite/MeshWeaver/Models/Paths/Modifiers/PathChain.cs
@@ -18,9 +18,6 @@ namespace Silksprite.MeshWeaver.Models.Paths.Modifiers
             if (pathie.Vertices.Count <= 1) return pathie;
 
             var vertices = pathie.Vertices.ToArray();
-            // FIXME: last is only an "end bone", how can we get rid of this without culling?
-            var lastVertex = vertices[vertices.Length - 1];
-            var last = new Vertie(Matrix4x4.TRS(lastVertex.Translation.GetPosition(), lastVertex.Translation.rotation, Vector3.zero), lastVertex.Uvs);
             var chain = vertices.Pairwise((a, b) => (a, b)).Select((ab, i) =>
                 {
                     var (a, b) = ab;
@@ -28,7 +25,7 @@ namespace Silksprite.MeshWeaver.Models.Paths.Modifiers
                     var mag = d.magnitude;
                     var m = Matrix4x4.TRS(a.Vertex, mag == 0 ? Quaternion.identity : Quaternion.LookRotation(d) * Quaternion.AngleAxis(_rolling * i, Vector3.forward), new Vector3(1, 1, mag));
                     return new Vertie(m, b.Uvs);
-                }).Concat(Enumerable.Repeat(last, 1));
+                });
             return new Pathie(chain, pathie.isLoop);
         }
     }
