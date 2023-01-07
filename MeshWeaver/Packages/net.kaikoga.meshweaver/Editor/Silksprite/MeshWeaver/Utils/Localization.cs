d98cbd5f7db8cb38b60f567e7cfc9852c5984e2d
kaikoga <info@mail.kaikoga.net>
2023-01-08 03:28:34 +0900

    refactor: caching

--- MeshWeaver/Packages/net.kaikoga.meshweaver/Editor/Silksprite/MeshWeaver/Utils/Localization.cs
+++ MeshWeaver/Packages/net.kaikoga.meshweaver/Editor/Silksprite/MeshWeaver/Utils/Localization.cs
@@ -7,47 +7,42 @@ namespace Silksprite.MeshWeaver.Utils
 {
     public static class Localization
     {
-        static string _lang;
-        static LocalizationAsset _po; 
-        static LocalizationAsset Po => _po ? _po : _po = AssetDatabase.LoadAssetAtPath<LocalizationAsset>(PoPath(_lang));
-
-        static Dictionary<string, string> _trCache;
-        static Dictionary<string, GUIContent> _guiContentCache;
-
         static string PoPath(string lang)
         {
             return $"Packages/net.kaikoga.meshweaver/Editor/Silksprite/MeshWeaver/{lang}.po";
         }
 
-        public static string Tr(string key)
-        {
-            if (_lang != MeshWeaverSettings.Current.lang)
-            {
-                _po = null;
-                _lang = MeshWeaverSettings.Current.lang;
-                _trCache = new Dictionary<string, string>();
-                _guiContentCache = new Dictionary<string, GUIContent>();
-            }
+        static readonly LocalizationCache<LocalizationAsset> PoCache = new LocalizationCache<LocalizationAsset>();
+        static LocalizationAsset Po => PoCache.FindOrCreate("po", (lang, key) => AssetDatabase.LoadAssetAtPath<LocalizationAsset>(PoPath(lang)));
 
-            if (_trCache.TryGetValue(key, out var tr)) return tr;
-            var po = Po;
-            tr = po != null ? po.GetLocalizedString(key) : key;
-            _trCache.Add(key, tr);
-            return tr;
-        }
-        
-        public static GUIContent GUIContent(string key)
+        static readonly LocalizationCache<string> TrCache = new LocalizationCache<string>();
+        static readonly LocalizationCache<GUIContent> GUIContentCache = new LocalizationCache<GUIContent>();
+
+        public static string Tr(string key) => TrCache.FindOrCreate(key, (l, k) => Po != null ? Po.GetLocalizedString(k) : k);
+
+        public static GUIContent GUIContent(string key) => GUIContentCache.FindOrCreate(key, (l, k) => new GUIContent(Po.GetLocalizedString(k), null, k));
+
+        public static LocalizedContent Loc(string key) => new LocalizedContent(key);
+
+        class LocalizationCache<T>
         {
-            var tr = Tr(key); // Invalidate cache
-            if (_guiContentCache.TryGetValue(key, out var guiContent)) return guiContent;
-            guiContent = new GUIContent(tr, null, key);
-            _guiContentCache.Add(key, guiContent);
-            return guiContent;
-        }
-        
-        public static LocalizedContent Loc(string key)
-        {
-            return new LocalizedContent(key);
+            string _lang;
+            Dictionary<string, T> _cache = new Dictionary<string, T>();
+
+            public delegate T Generator(string lang, string key); 
+
+            public T FindOrCreate(string key, Generator generator)
+            {
+                if (_lang != MeshWeaverSettings.Current.lang)
+                {
+                    _lang = MeshWeaverSettings.Current.lang;
+                    _cache = new Dictionary<string, T>();
+                }
+                if (_cache.TryGetValue(key, out var value)) return value;
+                value = generator(_lang, key);
+                _cache.Add(key, value);
+                return value;
+            }
         }
     }
 }
