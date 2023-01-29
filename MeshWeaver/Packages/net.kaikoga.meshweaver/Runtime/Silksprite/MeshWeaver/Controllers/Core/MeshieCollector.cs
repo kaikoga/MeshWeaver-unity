75950dbb4f1fb6777ea637f2ac48d75ba3b2c250
kaikoga <info@mail.kaikoga.net>
2023-01-29 15:26:02 +0900

    remove extension method

--- MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Core/MeshieCollector.cs
+++ MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Core/MeshieCollector.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Silksprite.MeshWeaver.Controllers.Base;
 using Silksprite.MeshWeaver.Controllers.Extensions;
 using Silksprite.MeshWeaver.Models.Meshes;
@@ -7,8 +8,25 @@ namespace Silksprite.MeshWeaver.Controllers.Core
 {
     public class MeshieCollector
     {
-        public IMeshieFactory CollectMeshie(MeshProvider meshProvider) => meshProvider.CollectMeshie();
+        public IMeshieFactory CollectMeshie(MeshProvider meshProvider)
+        {
+            if (meshProvider == null) return MeshieFactory.Empty;
 
-        public IMeshieFactory CollectMeshies(IEnumerable<MeshProvider> meshProviders) => meshProviders.CollectMeshies();
+            var localTranslation = meshProvider.transform.ToLocalTranslation();
+            return CompositeMeshieFactory.Builder().Concat(meshProvider.ToFactory(), localTranslation).ToFactory();
+        }
+
+        public IMeshieFactory CollectMeshies(IEnumerable<MeshProvider> meshProviders)
+        {
+            var builder = CompositeMeshieFactory.Builder();
+
+            foreach (var meshProvider in meshProviders.Where(c => c != null && c.gameObject.activeSelf))
+            {
+                var localTranslation = meshProvider.transform.ToLocalTranslation();
+                builder.Concat(meshProvider.ToFactory(), localTranslation);
+            }
+
+            return builder.ToFactory();
+        }
     }
 }
--- MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Core/PathieCollector.cs
+++ MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Core/PathieCollector.cs
@@ -2,15 +2,34 @@ using System.Collections.Generic;
 using System.Linq;
 using Silksprite.MeshWeaver.Controllers.Base;
 using Silksprite.MeshWeaver.Controllers.Extensions;
-using Silksprite.MeshWeaver.Models.Meshes;
+using Silksprite.MeshWeaver.Models.Modifiers;
 using Silksprite.MeshWeaver.Models.Paths;
+using Silksprite.MeshWeaver.Models.Paths.Core;
 
 namespace Silksprite.MeshWeaver.Controllers.Core
 {
     public class PathieCollector
     {
-        public IPathieFactory CollectPathie(PathProvider pathProvider) => pathProvider.CollectPathie();
+        public IPathieFactory CollectPathie(PathProvider pathProvider)
+        {
+            if (pathProvider == null) return PathieFactory.Empty;
 
-        public IPathieFactory CollectPathies(IEnumerable<PathProvider> pathProviders, bool isLoop, bool smoothJoin) => pathProviders.CollectPathies(isLoop, smoothJoin);
+            var localTranslation = pathProvider.transform.ToLocalTranslation();
+            // FIXME: maybe we want to cleanup Pathie.isLoop and fix this 
+            return ModifiedPathieFactory.Builder(pathProvider.ToFactory(), pathProvider.lodMask).Concat(new VertwiseTranslate(localTranslation)).ToFactory();
+        }
+
+        public IPathieFactory CollectPathies(IEnumerable<PathProvider> pathProviders, bool isLoop, bool smoothJoin)
+        {
+            var builder = CompositePathieFactory.Builder(isLoop, smoothJoin);
+            
+            foreach (var pathProvider in pathProviders.Where(c => c != null && c.gameObject.activeSelf))
+            {
+                var localTranslation = pathProvider.transform.ToLocalTranslation();
+                builder.Concat(pathProvider.ToFactory(), localTranslation);
+            }
+
+            return builder.ToFactory();
+        }
     }
 }
