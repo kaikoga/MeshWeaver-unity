07e4550e9cf3e68acd1e25d2d309d119cee2b4c4
kaikoga <info@mail.kaikoga.net>
2023-01-30 21:55:37 +0900

    collect modifiers through collector

--- MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Base/MeshProvider.cs
+++ MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Base/MeshProvider.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Silksprite.MeshWeaver.Controllers.Base.Modifiers;
+using Silksprite.MeshWeaver.Controllers.Core;
 using Silksprite.MeshWeaver.Models;
 using Silksprite.MeshWeaver.Models.Meshes;
 using Silksprite.MeshWeaver.Models.Meshes.Core;
@@ -10,17 +11,17 @@ namespace Silksprite.MeshWeaver.Controllers.Base
     {
         public LodMask lodMask = LodMask.All;
 
+        readonly ModifierCollector<IMeshModifierProvider> _modifierCollector = new ModifierCollector<IMeshModifierProvider>();
+
         public IMeshieFactory LastFactory => CachedObject;
 
         public IMeshieFactory ToFactory() => FindOrCreateObject();
 
-        protected sealed override int Sync() => SyncReferences() ^ GetComponents<IMeshModifierProvider>().Where(provider => provider.enabled).Aggregate(0, (r, content) => r ^ content.Revision);
+        protected sealed override int Sync() => SyncReferences() ^ _modifierCollector.Sync(GetComponents<IMeshModifierProvider>().Where(provider => provider.enabled)); 
 
         protected sealed override IMeshieFactory CreateObject()
         {
-            var providers = GetComponents<IMeshModifierProvider>()
-                .Where(provider => provider.enabled);
-            return providers.Aggregate(ModifiedMeshieFactory.Builder(CreateFactory(), lodMask),
+            return _modifierCollector.Value.Aggregate(ModifiedMeshieFactory.Builder(CreateFactory(), lodMask),
                     (builder, provider) => builder.Concat(provider.MeshieModifier, provider.LodMask))
                 .ToFactory();
         }
--- MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Base/PathProvider.cs
+++ MeshWeaver/Packages/net.kaikoga.meshweaver/Runtime/Silksprite/MeshWeaver/Controllers/Base/PathProvider.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Silksprite.MeshWeaver.Controllers.Base.Modifiers;
+using Silksprite.MeshWeaver.Controllers.Core;
 using Silksprite.MeshWeaver.Models;
 using Silksprite.MeshWeaver.Models.Paths;
 using Silksprite.MeshWeaver.Models.Paths.Core;
@@ -10,17 +11,17 @@ namespace Silksprite.MeshWeaver.Controllers.Base
     {
         public LodMask lodMask = LodMask.All;
 
+        readonly ModifierCollector<IPathModifierProvider> _modifierCollector = new ModifierCollector<IPathModifierProvider>();
+
         public IPathieFactory LastFactory => CachedObject;
 
         public IPathieFactory ToFactory() => FindOrCreateObject();
 
-        protected sealed override int Sync() => SyncReferences() ^ GetComponents<IPathModifierProvider>().Where(provider => provider.enabled).Aggregate(0, (r, content) => r ^ content.Revision);
+        protected sealed override int Sync() => SyncReferences() ^ _modifierCollector.Sync(GetComponents<IPathModifierProvider>().Where(provider => provider.enabled));
 
         protected sealed override IPathieFactory CreateObject()
         {
-            var providers = GetComponents<IPathModifierProvider>()
-                .Where(provider => provider.enabled);
-            return providers.Aggregate(ModifiedPathieFactory.Builder(CreateFactory(), lodMask),
+            return _modifierCollector.Value.Aggregate(ModifiedPathieFactory.Builder(CreateFactory(), lodMask),
                     (builder, provider) => builder.Concat(provider.PathieModifier, provider.LodMask))
                 .ToFactory();
         }
